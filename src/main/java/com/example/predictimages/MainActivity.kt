2705376b5b6be092a4699a419dac8273069e8de8
Wissam Mousa <119519456+WessamM1@users.noreply.github.com>
2023-12-29 15:33:35 +0200

    Add files via upload

---
+++ src/main/java/com/example/predictimages/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.predictimages
+
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.*
+import com.example.predictimages.ml.MobilenetV110224Quant
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.common.ops.NormalizeOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.image.ops.TransformToGrayscaleOp
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var selectBtn: Button
+    lateinit var predBtn: Button
+    lateinit var resView: TextView
+    lateinit var imageView: ImageView
+    lateinit var bitmap: Bitmap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        selectBtn = findViewById(R.id.selectBtn)
+        predBtn = findViewById(R.id.PredictBtn)
+        resView = findViewById(R.id.resView)
+        imageView = findViewById(R.id.imageView)
+
+
+        var labels = application.assets.open("labels.txt").bufferedReader().readLines()
+
+
+        // image processor
+        var imageProcessor = ImageProcessor.Builder()
+//            .add(NormalizeOp(0.0f,255.0f))
+//            .add(TransformToGrayscaleOp())
+            .add(ResizeOp(224,224,ResizeOp.ResizeMethod.BILINEAR))
+            .build()
+
+        selectBtn.setOnClickListener{
+            var intent = Intent()
+            intent.setAction(Intent.ACTION_GET_CONTENT)
+            intent.setType("image/*")
+            startActivityForResult(intent , 100)
+        }
+
+        predBtn.setOnClickListener{
+
+            var tensorImage = TensorImage(DataType.UINT8)
+            tensorImage.load(bitmap)
+
+            tensorImage = imageProcessor.process(tensorImage)
+
+            val model = MobilenetV110224Quant.newInstance(this)
+
+            // Creates inputs for reference.
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.UINT8)
+            inputFeature0.loadBuffer(tensorImage.buffer)
+
+            // Runs model inference and gets result.
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
+
+            var maxIdx = 0
+            outputFeature0.forEachIndexed{ index, fl ->
+                if(outputFeature0[maxIdx] < fl){
+                    maxIdx = index
+                }
+            }
+
+            resView.setText(labels[maxIdx])
+
+            // Releases model resources if no longer used.
+            model.close()
+
+
+        }
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 100) {
+            var uri = data?.data;
+            bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+            imageView.setImageBitmap(bitmap)
+
+        }
+    }
+}
